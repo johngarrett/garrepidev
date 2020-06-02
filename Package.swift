caf1e09cbb756053f096a51eae5eeb5d538cb752
John Garrett <jacgarrett18@gmail.com>
2020-06-02 00:00:17 -0400

    remove parent dir

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "website",
+    name: "garrepi.dev",
     dependencies: [
         .package(name: "Ink", url: "https://github.com/johnsundell/ink.git", from: "0.1.0")
     ],
@@ -12,10 +12,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "website",
+            name: "garrepi.dev",
             dependencies: ["Ink"]),
         .testTarget(
             name: "websiteTests",
-            dependencies: ["website"]),
+            dependencies: ["garrepi.dev"]),
     ]
 )
