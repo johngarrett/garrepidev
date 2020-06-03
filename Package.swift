3fb179ba0e83e6f4710fff6cc5a0dcdf89ad734c
John Garrett <jacgarrett18@gmail.com>
2020-06-02 20:02:25 -0400

    rename and add platform

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "garrepi.dev",
+    name: "garrepidev",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(
-            name: "garrepi.dev",
-            targets: ["garrepi.dev"]
+            name: "garrepidev",
+            targets: ["garrepidev"]
         ),
     ],
     dependencies: [
@@ -17,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "garrepi.dev",
+            name: "garrepidev",
             dependencies: ["Ink"]),
     ]
 )
--- README.md
+++
@@ -1,3 +0,0 @@
-# website
-
-A description of this package.
