7b893e9aaded05767a7a43bbaa704129a2d63b39
John Garrett <jacgarrett18@gmail.com>
2020-10-13 14:59:08 -0400

    trtracking and new style sheet

--- Sources/garrepidev/Pages/Root.swift
+++ Sources/garrepidev/Pages/Root.swift
@@ -45,6 +45,20 @@ public class Head: HTMLComponent {
             \(stylesheets.joined(separator: "\n"))
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.css">
           <title>\(title)</title>
+            <!-- Analytical info, you can disable this if you'd like -->
+            <script type="text/javascript">
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://garrepidev.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/garrepidev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            </script>
         </head>
         """
     }
